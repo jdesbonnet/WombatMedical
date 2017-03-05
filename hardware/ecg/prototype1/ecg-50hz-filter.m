0a78c2ed836db9c40a3a032196919a33008e6089
Joe Desbonnet <jdesbonnet@gmail.com>
2017-03-05 22:46:10 +0000

    Octave/MATLAB script to filter 50Hz mains noise from ECG

---
+++ hardware/ecg/prototype1/ecg-50hz-filter.m
@@ -0,0 +1,34 @@
+#!/usr/bin/env octave -q
+#
+# Remove 50Hz mains noise from ECG. Assuming 500sps sample rate.
+#
+# Command line:
+# octave  ecg-50hz-filter.m  ecg.dat ecg-filtered.dat > /dev/null
+#
+# Joe Desbonnet,
+# jdesbonnet@gmail.com
+# 5 Mar 2017
+
+datain = argv(){1}
+dataout = argv(){2}
+
+ecg_data = dlmread(datain, " ")
+
+# Extract column #1 (channel #1) as vector
+ecg_ch1 = ecg_data( : , 1)
+
+# Sample rate 500Hz.
+sps=500
+
+# Stop band frequencies are normalized to nyquist frequency. 
+# ie f=1.0 means nyquist frequency.
+nyquist_f = sps/2
+f1 = (50-6)/nyquist_f
+f2 = (50+6)/nyquist_f
+
+stop50hz = fir1(128, [f1 f2], 'stop')
+
+ecg_ch1_filtered = filter(stop50hz,1,ecg_ch1)
+
+dlmwrite (dataout, ecg_ch1_filtered, " " )
+
